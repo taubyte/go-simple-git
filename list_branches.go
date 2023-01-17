7415f0fe1432177cce0d72fbb256f06d476efed5
Samuel Stoltenberg <sam@skelouse.com>
2023-01-17 14:09:58 -0600

    Add list branches call

---
+++ list_branches.go
@@ -0,0 +1,61 @@
+package gosimplegit
+
+import (
+	"fmt"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+)
+
+/*
+ListBranches will return a list of branches for the repository
+
+fetch true will search remote origin to gather all branches
+fetch false will search .git/config to gather branches
+*/
+func (c *Repository) ListBranches(fetch bool) (branches []string, fetchErr error, err error) {
+	if fetch == true {
+		return c.fetchAndListBranches()
+	}
+
+	branchRef, err := c.repo.Branches()
+	if err != nil {
+		return nil, fetchErr, fmt.Errorf("listing branches for `%s` failed with: %s", c.url, err)
+	}
+
+	branches = make([]string, 0)
+	err = branchRef.ForEach(func(r *plumbing.Reference) error {
+		branches = append(branches, r.Name().Short())
+		return nil
+	})
+	if err != nil {
+		return nil, fetchErr, fmt.Errorf("for each branch for `%s` failed with: %s", c.url, err)
+	}
+
+	return branches, fetchErr, nil
+}
+
+func (c *Repository) fetchAndListBranches() (branches []string, fetchErr error, err error) {
+	fetchErr = c.Fetch()
+
+	rem, err := c.repo.Remote("origin")
+	if err != nil {
+		return nil, fetchErr, err
+	}
+
+	remoteLister, err := rem.List(&git.ListOptions{
+		Auth: c.auth,
+	})
+	if err != nil {
+		return nil, fetchErr, err
+	}
+
+	branches = make([]string, 0)
+	for _, branch := range remoteLister {
+		if branch.Name().IsBranch() == true {
+			branches = append(branches, branch.Name().Short())
+		}
+	}
+
+	return
+}
